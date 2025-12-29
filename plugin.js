b132c6be767136553318c41eeec5e017cb142492
rellisCC <130396690+rellisCC@users.noreply.github.com>
2025-12-29 16:18:40 -0500

    Update plugin.js
    wording

--- plugin.js
+++ plugin.js
@@ -1291,21 +1291,21 @@ els.deltaInfo.innerHTML = `
     <div class="mathline"><b>Old model: ${fmtRule(w1Old, w2Old, cOld, false)}</b></div>
     
   <div class="mathblock">
-    <div class="mathline"><b>w1 shift</b></div>
+    <div class="mathline"><b>w1 adjustment</b></div>
     <div class="mathline small">New w1 = Old w1 + LearnRate × TrueSentiment × feat1</div>
     <div class="mathline small">New w1 = ${w1Old.toFixed(2)} + ${lr.toFixed(2)} × (${y}) × ${pt.feat1}</div>
     <div class="mathline small"><b> New w1 = ${model.w1.toFixed(2)}</b></div>
   </div>
 
   <div class="mathblock">
-    <div class="mathline"><b>w2 shift</b></div>
+    <div class="mathline"><b>w2 adjustment</b></div>
     <div class="mathline small">New w2 = Old w2 + LearnRate × TrueSentiment × feat2</div>
     <div class="mathline small"> New w2 = ${w2Old.toFixed(2)} + ${lr.toFixed(2)} × (${y}) × ${pt.feat2}</div>
     <div class="mathline small"><b>New w2 = ${model.w2.toFixed(2)}</b></div>
   </div>
 
   <div class="mathblock">
-    <div class="mathline"><b>c shift</b></div>
+    <div class="mathline"><b>c adjustment</b></div>
     <div class="mathline small">New c = Old c + LearnRate × TrueSentiment</div>
     <div class="mathline small">New c = ${cOld.toFixed(2)} + ${lr.toFixed(2)} × (${y})</div>
     <div class="mathline small"><b> New c = ${model.c.toFixed(2)}</b></div>
