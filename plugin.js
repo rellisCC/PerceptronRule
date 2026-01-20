2d19af3f6c7b033bf85861b73b76b90b5818fcae
rellisCC <130396690+rellisCC@users.noreply.github.com>
2026-01-20 11:48:28 -0500

    Update plugin.js
    change label for TrueLabel to ErrorType

--- plugin.js
+++ plugin.js
@@ -1590,21 +1590,21 @@ els.deltaInfo.innerHTML = `
     
   <div class="mathblock">
     <div class="mathline"><b>w1 adjustment</b></div>
-    <div class="mathline small">New w1 = Old w1 + LearnRate × TrueLabel × feat1</div>
+    <div class="mathline small">New w1 = Old w1 + LearnRate × ErrorType × feat1</div>
     <div class="mathline small">New w1 = ${w1Old.toFixed(2)} + ${lr.toFixed(2)} × (${y}) × ${pt.feat1}</div>
     <div class="mathline small"><b> New w1 = ${model.w1.toFixed(2)}</b></div>
   </div>
 
   <div class="mathblock">
     <div class="mathline"><b>w2 adjustment</b></div>
-    <div class="mathline small">New w2 = Old w2 + LearnRate × TrueLabel × feat2</div>
+    <div class="mathline small">New w2 = Old w2 + LearnRate × ErrorType × feat2</div>
     <div class="mathline small"> New w2 = ${w2Old.toFixed(2)} + ${lr.toFixed(2)} × (${y}) × ${pt.feat2}</div>
     <div class="mathline small"><b>New w2 = ${model.w2.toFixed(2)}</b></div>
   </div>
 
   <div class="mathblock">
     <div class="mathline"><b>c adjustment</b></div>
-    <div class="mathline small">New c = Old c + LearnRate × TrueLabel</div>
+    <div class="mathline small">New c = Old c + LearnRate × ErrorType</div>
     <div class="mathline small">New c = ${cOld.toFixed(2)} + ${lr.toFixed(2)} × (${y})</div>
     <div class="mathline small"><b> New c = ${model.c.toFixed(2)}</b></div>
   </div>
